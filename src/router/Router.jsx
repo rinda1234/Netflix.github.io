4dcfbaa9eb0d40cdfb18aea01a61bd3cdbd8c598
rinda1234 <84953568+rinda1234@users.noreply.github.com>
2025-12-15 11:44:37 +0900

    add protected route for auth

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,16 +4,49 @@ import Popular from "../pages/Popular";
 import Search from "../pages/Search";
 import Wishlist from "../pages/Wishlist";
 import SigninPage from "../pages/SigninPage";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/signin" element={<SigninPage />} />
-                <Route path="/" element={<Home />} />
-                <Route path="/popular" element={<Popular />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/wishlist" element={<Wishlist />} />
+
+                <Route
+                    path="/"
+                    element={
+                        <ProtectedRoute>
+                            <Home />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/popular"
+                    element={
+                        <ProtectedRoute>
+                            <Popular />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/search"
+                    element={
+                        <ProtectedRoute>
+                            <Search />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/wishlist"
+                    element={
+                        <ProtectedRoute>
+                            <Wishlist />
+                        </ProtectedRoute>
+                    }
+                />
             </Routes>
         </BrowserRouter>
     );
