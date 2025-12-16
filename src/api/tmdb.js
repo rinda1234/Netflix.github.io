2695824be37985a585dffc9cc6ee015d00d07608
rinda1234 <84953568+rinda1234@users.noreply.github.com>
2025-12-16 11:42:03 +0900

    Add Tdbm key

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -2,8 +2,11 @@ import axios from "axios";
 
 const tmdb = axios.create({
     baseURL: "https://api.themoviedb.org/3",
+    headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`,
+        "Content-Type": "application/json;charset=utf-8",
+    },
     params: {
-        api_key: import.meta.env.VITE_TMDB_API_KEY,
         language: "ko-KR",
     },
 });
